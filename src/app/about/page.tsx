880c0121ab312119e8723d0771c5545d57b8f5d1
Matias Torres <matiastorressuarez1@gmail.com>
2024-09-07 18:20:22 -0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='ztext'>
+        <h3>About</h3>
+        <p>This software was created to provide a solution to the lack of sites with to ability to create powerfull charts, it was founded in Mexico, City by founder @MatiasTorres, the tool is 100% free unless you use one of our subscription plans witch serve a purpose of best posible experience when creating charts but you can still use it for free to its full extent.</p>
+    </div>
+  )
+}
+
+export default page
